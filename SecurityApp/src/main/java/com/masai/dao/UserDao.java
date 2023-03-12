539ed04374ee1e8d37e1dbf178d89e786be7570d
ABHISHEK KUMAR <107480660+hustler-abhi@users.noreply.github.com>
2023-03-12 22:31:51 +0530

    Update UserDao.java

--- SecurityApp/src/main/java/com/masai/dao/UserDao.java
+++ SecurityApp/src/main/java/com/masai/dao/UserDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.masai.entity.User;
 
-public interface UserDao extends JpaRepository<User,Integer>{
+public interface UserDao extends  JpaRepository<User,Integer>{
 	
 	public User findByEmail(String email);
  
