f45b75d93a35bea976b789bf719c66ca4490c89f
ABHISHEK KUMAR <107480660+hustler-abhi@users.noreply.github.com>
2023-03-17 23:04:55 +0530

    Update AppConfig.java

--- SecurityApp/src/main/java/com/masai/configuration/AppConfig.java
+++ SecurityApp/src/main/java/com/masai/configuration/AppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = true) 
 public class AppConfig {
 
 	@Bean
